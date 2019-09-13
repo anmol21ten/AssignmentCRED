ed8a8d78b8a373d5f4dce9011a938033e28420cc
anmol21ten <37690595+anmol21ten@users.noreply.github.com>
2019-09-13 09:39:19 +0530

    Delete MaximumGainService.java

--- main/java/com/cred/assignment/services/MaximumGainService.java
+++
@@ -1,8 +0,0 @@
-package com.cred.assignment.services;
-
-import com.cred.assignment.exchanges.MaximumGainRequest;
-import com.cred.assignment.exchanges.MaximumGainResponse;
-
-public interface MaximumGainService {
-    MaximumGainResponse getMaximumGain(MaximumGainRequest maximumGainRequest);
-}
